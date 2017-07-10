a474d3bc4ced2f5e7f91397110f5bf4aa63c6c84
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2017-07-11 02:31:39 +0900

    Add show action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+    def not_found(exception)
+      model = build_error_model(exception.model, :id, 'Not found')
+      render json: model.errors, status: :not_found
+    end
+
+    def build_error_model(model_name, attribute, message)
+      model = model_name.classify.constantize.new
+      model.errors.add(attribute, message)
+      model
+    end
 end
--- spec/acceptance/todo_spec.rb
+++ spec/acceptance/todo_spec.rb
@@ -22,4 +22,27 @@ resource 'Todos' do
       end
     end
   end
+
+  route '/todos/:id', 'Get a todo' do
+    get 'Get a todo' do
+      let(:id) { todo.id }
+      let(:todo) { create(:todo) }
+      let(:expected_json) { TodoSerializer.new(todo).to_json }
+
+      example_request 'exists' do
+        expect(response_body).to eq expected_json
+        expect(status).to eq 200
+      end
+    end
+
+    get 'Get a todo' do
+      let(:id) { 0 }
+      let(:expected_json) { {id: ['Not found']}.to_json }
+
+      example_request 'not exists' do
+        expect(response_body).to eq expected_json
+        expect(status).to eq 404
+      end
+    end
+  end
 end
